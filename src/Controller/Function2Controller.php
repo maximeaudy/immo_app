4274f31ad3936965c2bd3220056583ad77af572a
lmarcouiller <lucas.marcouiller@etu.u-bordeaux.fr>
2020-11-16 18:00:02 +0100

    push controller et template function2

--- src/Controller/Function2Controller.php
+++ src/Controller/Function2Controller.php
@@ -2,6 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController
+class Function2Controller extends AbstractController
+{
+    /**
+     * @Route("/function2", name="function2")
+     */
+    public function function2()
+    {
+
+        $number = $this->random();
+        return $this->render('function2/function2.html.twig', [
+            'number' => $number,
+        ]);
+    }
+    private function random()
+    {
+        return random_int(0, 100);
+    }
+}
