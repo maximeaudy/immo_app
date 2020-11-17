d4a4db4866eed4e50cf5116d33f4345f3be8d089
lmarcouiller <lucas.marcouiller@etu.u-bordeaux.fr>
2020-11-17 10:54:52 +0100

    test api valuer fonciere

--- src/Controller/Function2Controller.php
+++ src/Controller/Function2Controller.php
@@ -12,10 +12,20 @@ class Function2Controller extends AbstractController
      */
     public function function2()
     {
-
-        $number = $this->random();
+        $url = 'http://api.cquest.org/dvf';
+        $collection_name= 'numero_plan=94068000CQ0110';
+        $request_url= $url.'?'.$collection_name;
+        $curl = curl_init($request_url);
+        curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json'
+        ]);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        $response = json_decode($response);
+        echo var_dump($response->{'valeur_fonciere'});
         return $this->render('function2/function2.html.twig', [
-            'number' => $number,
+
         ]);
     }
     private function random()
