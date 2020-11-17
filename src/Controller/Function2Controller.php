e4911b3a0c015438ac5daa5864ad7aedf80fcc1f
lmarcouiller <lucas.marcouiller@etu.u-bordeaux.fr>
2020-11-17 12:22:45 +0100

    essai faire un form

--- src/Controller/Function2Controller.php
+++ src/Controller/Function2Controller.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\Type\Function2Type;
 
 class Function2Controller extends AbstractController
 {
@@ -12,8 +14,19 @@ class Function2Controller extends AbstractController
      */
     public function function2()
     {
-        $url = 'http://api.cquest.org/dvf';
+
         $collection_name= 'numero_plan=94068000CQ0110';
+        $response = $this->get_response($collection_name);
+
+        print $this->prix_metre_carre($response);
+        return $this->render('function2/function2.html.twig', [
+
+        ]);
+    }
+
+    private function get_response($collection_name){
+        $url = 'http://api.cquest.org/dvf';
+
         $request_url= $url.'?'.$collection_name;
         $curl = curl_init($request_url);
         curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
@@ -22,14 +35,17 @@ class Function2Controller extends AbstractController
         ]);
         $response = curl_exec($curl);
         curl_close($curl);
-        $response = json_decode($response);
-        echo var_dump($response->{'valeur_fonciere'});
-        return $this->render('function2/function2.html.twig', [
+        return $response= json_decode($response);
 
-        ]);
     }
-    private function random()
-    {
-        return random_int(0, 100);
+
+    private function prix_metre_carre($response){
+        $surface = $response->{'resultats'}[0]->{'surface_terrain'};
+        $valeur_fonciere = $response->{'resultats'}[0]->{'valeur_fonciere'};
+        if ($surface == 0){
+            $surface = $surface = $response->{'resultats'}[0]->{'surface_relle_bati'};
+        }
+        $prix_metre_carre = round($valeur_fonciere/$surface,3);
+        return $prix_metre_carre;
     }
 }
