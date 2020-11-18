25c734e5016a320e02fbea7c67cf16c708254c07
Senniinn <damien.genevee@epsi.fr>
2020-11-18 11:51:42 +0100

    feat : proto ok

--- src/Controller/EstimateController.php
+++ src/Controller/EstimateController.php
@@ -11,6 +11,22 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class EstimateController extends AbstractController
 {
+     private function array_median($arr) {
+        if(empty($arr)){
+            return false;
+        }
+        $num = count($arr);
+        $middleVal = floor(($num - 1) / 2);
+        if($num % 2) {
+            return $arr[$middleVal];
+        }
+        else {
+            $lowMid = $arr[$middleVal];
+            $highMid = $arr[$middleVal + 1];
+            return (($lowMid + $highMid) / 2);
+        }
+    }
+
     /**
      * @Route("/estimate", name="estimate")
      */
@@ -61,46 +77,65 @@ class EstimateController extends AbstractController
                 var_dump('aucun resultat');die;
             }
 
+            $delta = null;
+            if($type == 1) {
+                $delta = 10;
+            } elseif($type == 2) {
+                $delta = 2;
+            }
+
             $total_property = 0;
-            $total_price = 0;
-            $total_property2014 = 0;
-            $total_price2014 = 0;
             $total_property2018 = 0;
-            $total_price2018 = 0;
-            $price_average_2014 = 0;
+            $total_property2019 = 0;
+            $tab_all_price = [];
+            $tab_2018_all_price = [];
+            $tab_2019_all_price = [];
             $price_average_2018 = 0;
+            $price_average_2019 = 0;
+
             foreach ($response['resultats'] as $propertySale){
-                if(($propertySale['surface_relle_bati'] == $area || is_null($area)) &&
+                $date = explode("-", $propertySale['date_mutation']);
+                if(($propertySale['surface_relle_bati'] <= $area + $delta || is_null($area)) &&
+                    ($propertySale['surface_relle_bati'] >= $area - $delta || is_null($area)) &&
+                    ((int)$date[0] >= (int)"2018") &&
                     ($propertySale['surface_terrain'] == $surface || is_null($surface)) &&
                     ($propertySale['nombre_pieces_principales'] == $numberRoom || is_null($numberRoom))&&
                     ($propertySale['code_type_local'] == $type || is_null($type)) &&
                     ($propertySale['section'] == $section || is_null($section))) {
                     $total_property++;
-                    $total_price += $propertySale['valeur_fonciere'];
-                    $date = explode("-", $propertySale['date_mutation']);
-                    if ($date[0] === "2014")  {
-                        $total_property2014++;
-                        $total_price2014 += $propertySale['valeur_fonciere'];
-                        $price_average_2014 = round($total_price2014 / $total_property2014, 0);;
-                    } else if ($date[0] === "2018") {
+                    $tab_all_price[] = $propertySale['valeur_fonciere'];
+                    if ($date[0] === "2018")  {
                         $total_property2018++;
-                        $total_price2018 += $propertySale['valeur_fonciere'];
-                        $price_average_2018 = round($total_price2018 / $total_property2018, 0);;
+                        $tab_2018_all_price[] = $propertySale['valeur_fonciere'];
+                    } else if ($date[0] === "2019") {
+                        $total_property2019++;
+                        $tab_2019_all_price[] = $propertySale['valeur_fonciere'];
                     }
                 }
             }
-            $augmentation = $price_average_2018 / $price_average_2014;
 
-            $estimation_2022 = $price_average_2018 * $augmentation;
+            $augmentation = null;
+            if (!empty($tab_2018_all_price) && !empty($tab_2019_all_price)) {
+                sort($tab_2018_all_price);
+                $price_average_2018 = $this->array_median($tab_2018_all_price);
+                sort($tab_2019_all_price);
+                $price_average_2019 = $this->array_median($tab_2019_all_price);
+                $augmentation = $price_average_2019 / $price_average_2018;
+            }
 
 
             if($total_property == 0){
                 var_dump('aucun resultat correspondant à votre recherche');die;
             }
+            sort($tab_all_price);
+            $price = $this->array_median($tab_all_price);
 
-            $price = round($total_price / $total_property, 0);
+            if ($augmentation != null) {
+                $estimation_2_years_later = $price * $augmentation;
+            }
+
             if($modern){
-                $price = $price * 1.15;
+                $price = $price * 1.10;
             }
             if($transport){
                 $price = $price * 1.03;
@@ -116,7 +151,8 @@ class EstimateController extends AbstractController
 
         return $this->render('estimate/index.html.twig', [
             'estimatePriceForm' => $form->createView(),
-            'estimatePrice' => $price ?? null
+            'estimatePrice' => $price ?? null,
+            'estimatePrice2years' => $estimation_2_years_later ?? null,
         ]);
     }
 }
